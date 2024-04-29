91ea4c198fbf496c2008566da1f5e524ed252544
Parth Agarwal <ra101.dev@protonmail.com>
2024-04-29 15:02:16 +0530

    Create vscode_extension.cmd

---
+++ vscode_extension.cmd
@@ -0,0 +1,35 @@
+@REM Get this List: 
+@REM code --list-extensions | % { "code --install-extension $_" }
+
+code --install-extension batisteo.vscode-django
+code --install-extension bracketpaircolordlw.bracket-pair-color-dlw
+code --install-extension britesnow.vscode-toggle-quotes
+code --install-extension cardinal90.multi-cursor-case-preserve
+code --install-extension codezombiech.gitignore
+code --install-extension denoland.vscode-deno
+code --install-extension ecmel.vscode-html-css
+code --install-extension esbenp.prettier-vscode
+code --install-extension kisstkondoros.vscode-gutter-preview
+code --install-extension koichisasada.vscode-rdbg
+code --install-extension mhutchie.git-graph
+code --install-extension mintlify.document
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension natqe.reload
+code --install-extension oderwat.indent-rainbow
+code --install-extension pkief.material-icon-theme
+code --install-extension ritwickdey.liveserver
+code --install-extension shopify.ruby-extensions-pack
+code --install-extension shopify.ruby-lsp
+code --install-extension sorbet.sorbet-vscode-extension
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension unifiedjs.vscode-mdx
+code --install-extension vscjava.vscode-maven
+code --install-extension wallabyjs.console-ninja
+code --install-extension ziyasal.vscode-open-in-github
